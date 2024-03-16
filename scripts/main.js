a597e8c46529af5a7df8e06910bde986cf5e7f18
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2024-03-16 05:30:28 +0000

    Initial

---
+++ scripts/main.js
@@ -0,0 +1,91 @@
+(function() {
+  "use strict";
+
+  window.addEventListener('load', () => {
+    on_page_load()
+  });
+
+  /**
+   * Function gets called when page is loaded.
+   */
+  function on_page_load() {
+    // Initialize On-scroll Animations
+    AOS.init({
+      anchorPlacement: 'top-left',
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      disable: 'mobile'
+    });
+  }
+
+  /**
+   * Navbar effects and scrolltop buttons upon scrolling
+   */
+  const navbar = document.getElementById('header-nav')
+  var body = document.getElementsByTagName("body")[0]
+  const scrollTop = document.getElementById('scrolltop')
+  window.onscroll = () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add('fixed-top', 'shadow-sm')
+      body.style.paddingTop = navbar.offsetHeight + "px"
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+    } else {
+      navbar.classList.remove('fixed-top', 'shadow-sm')
+      body.style.paddingTop = "0px"
+      scrollTop.style.visibility = "hidden";
+      scrollTop.style.opacity = 0;
+    }
+  };
+
+  /**
+   * Masonry Grid
+   */
+  var elem = document.querySelector('.grid');
+  if(elem) {
+    imagesLoaded(elem, function() {
+      new Masonry(elem, {
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        horizontalOrder: true
+      });
+    })
+  }
+
+  /**
+   * Big Picture Popup for images and videos
+   */
+   document.querySelectorAll("[data-bigpicture]").forEach((function(e) {
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       const data =JSON.parse(e.dataset.bigpicture)
+       BigPicture({
+        el: t.target,
+        ...data
+      })
+     })
+    )
+  }))
+
+  // Add your javascript here
+
+  var currentPageUrl = window.location.href;
+
+  // Get all navigation links
+  var navLinks = document.querySelectorAll('.nav-link');
+
+  // Loop through each navigation link
+  navLinks.forEach(function(link) {
+    // If the link's href matches the current page URL, add 'active' class to highlight it
+    if (link.href === currentPageUrl) {
+      link.classList.add('active');
+    }
+  });
+
+  var iframe = document.getElementsByTagName('mygame')[0];
+  iframe.style.background = 'grey';
+  iframe.contentWindow.document.body.style.backgroundColor = 'grey';
+
+})();
